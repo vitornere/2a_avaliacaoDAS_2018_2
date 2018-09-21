cf853aba111a6fa64abfca0ca78281289d3e7c45
Vitor Nere <vitor.nere@hotmail.com>
2018-09-21 20:25:25 -0300

    Imposto de Renda barra vermelha novo teste

--- tests/ImpostoDeRendaTest.java
+++ tests/ImpostoDeRendaTest.java
@@ -6,12 +6,22 @@ import org.junit.Test;
 public class ImpostoDeRendaTest {
 
     @Test()
-    public void calculaImpostoDeRenda() {
+    public void calculaImpostoDeRenda60DiasE1000() {
         Investimento investimento = new Investimento(60, 1000.00, 8.5);
         CalculaInvestimento calculaInvestimento = new CalculaInvestimento(investimento);
 
         Double impostoDeRenda = calculaInvestimento.calculaImpostoDeRenda();
 
         Assert.assertEquals(3.14, impostoDeRenda, 0.01);
+    }
+
+    @Test()
+    public void calculaImpostoDeRenda120DiasE500() {
+        Investimento investimento = new Investimento(120, 500.0, 8.0);
+        CalculaInvestimento calculaInvestimento = new CalculaInvestimento(investimento);
+
+        Double impostoDeRenda = calculaInvestimento.calculaImpostoDeRenda();
+
+        Assert.assertEquals(2.96, impostoDeRenda, 0.01);
     }
 }
