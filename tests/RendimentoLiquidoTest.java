195e14b528a8d3b997327e38ec8b7dfc9f9bd88d
Vitor Nere <vitor.nere@hotmail.com>
2018-09-21 20:36:48 -0300

    Rendimento Liquido barra vemelha sem implementação

---
+++ tests/RendimentoLiquidoTest.java
@@ -0,0 +1,17 @@
+import controller.CalculaInvestimento;
+import model.Investimento;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RendimentoLiquidoTest {
+
+    @Test()
+    public void calculaRendimentoLiquido() {
+        Investimento investimento = new Investimento(60, 1000.00, 8.5);
+        CalculaInvestimento calculaInvestimento = new CalculaInvestimento(investimento);
+
+        Double rendimentoLiquido = calculaInvestimento.calculaRendimentoLiquido();
+
+        Assert.assertEquals(1.0829, rendimentoLiquido, 0.01);
+    }
+}
