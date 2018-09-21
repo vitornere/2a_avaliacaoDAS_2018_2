8b2898621390a4fd053d804ead61f4bd1109c03f
Vitor Nere <vitor.nere@hotmail.com>
2018-09-21 19:52:52 -0300

    Rendimento bruto barra vermelha novo teste

--- tests/RendimentoBrutoTest.java
+++ tests/RendimentoBrutoTest.java
@@ -6,12 +6,22 @@ import org.junit.Test;
 public class RendimentoBrutoTest {
 
     @Test()
-    public void calculaRendimentoBruto() {
+    public void calculaRendimentoBruto60DiasE1000() {
         Investimento investimento = new Investimento(60, 1000.00);
         CalculaInvestimento calculaInvestimento = new CalculaInvestimento(investimento);
 
         Double rendimentoBruto = calculaInvestimento.calculaRendimentoBruto();
 
         Assert.assertEquals(13.97, rendimentoBruto, 0.01);
+    }
+
+    @Test()
+    public void calculaRendimentoBruto120DiasE500() {
+        Investimento investimento = new Investimento(120, 500.0);
+        CalculaInvestimento calculaInvestimento = new CalculaInvestimento(investimento);
+
+        Double rendimentoBruto = calculaInvestimento.calculaRendimentoBruto();
+
+        Assert.assertEquals(13.15, rendimentoBruto, 0.01);
     }
 }
