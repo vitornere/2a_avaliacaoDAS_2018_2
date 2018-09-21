497e0ea31ce0caf7e8a55170d2d3d8207f6bba92
Vitor Nere <vitor.nere@hotmail.com>
2018-09-21 20:39:27 -0300

    Rendimento Liquido barra vermelha novo teste

--- tests/RendimentoLiquidoTest.java
+++ tests/RendimentoLiquidoTest.java
@@ -6,12 +6,22 @@ import org.junit.Test;
 public class RendimentoLiquidoTest {
 
     @Test()
-    public void calculaRendimentoLiquido() {
+    public void calculaRendimentoLiquido60DiasE1000() {
         Investimento investimento = new Investimento(60, 1000.00, 8.5);
         CalculaInvestimento calculaInvestimento = new CalculaInvestimento(investimento);
 
         Double rendimentoLiquido = calculaInvestimento.calculaRendimentoLiquido();
 
         Assert.assertEquals(1.0829, rendimentoLiquido, 0.0001);
+    }
+
+    @Test()
+    public void calculaRendimentoLiquido120DiasE500() {
+        Investimento investimento = new Investimento(120, 500.00, 8.0);
+        CalculaInvestimento calculaInvestimento = new CalculaInvestimento(investimento);
+
+        Double rendimentoLiquido = calculaInvestimento.calculaRendimentoLiquido();
+
+        Assert.assertEquals(2.0384, rendimentoLiquido, 0.0001);
     }
 }
